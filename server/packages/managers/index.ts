0bd361dcedba38a5429bddc3cff01202a2e5a924
Danishan Farookh <danishan089@gmail.com>
2026-03-15 21:16:53 +0530

    feat: Added managers to the index.ts

---
+++ server/packages/managers/index.ts
@@ -0,0 +1,4 @@
+export { OtpManager } from "./OtpManager";
+export { ReservationManager } from "./ReservationManager";
+export { otpManager, reservationManager } from "./instance";
+export { RedisManager } from "./RedisManager";
