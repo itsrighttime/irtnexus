4d2c269c7645450a436dcf0d3452cf3f6232b684
Danishan Farookh <danishan089@gmail.com>
2026-03-16 22:32:54 +0530

    feat: Added Storybook

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,22 +1,54 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      // {
-      //   find: "@components",
-      //   replacement: path.resolve(__dirname, "src/components"),
-      // },
-      // { find: "@layouts", replacement: path.resolve(__dirname, "src/layouts") },
-      // { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
-      // { find: "@routes", replacement: path.resolve(__dirname, "src/routes") },
-      // { find: "@stores", replacement: path.resolve(__dirname, "src/stores") },
-      // { find: "@widgets", replacement: path.resolve(__dirname, "src/widgets") },
-    ],
+    alias: [{
+      find: "@",
+      replacement: path.resolve(__dirname, "src")
+    }
+    // {
+    //   find: "@components",
+    //   replacement: path.resolve(__dirname, "src/components"),
+    // },
+    // { find: "@layouts", replacement: path.resolve(__dirname, "src/layouts") },
+    // { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+    // { find: "@routes", replacement: path.resolve(__dirname, "src/routes") },
+    // { find: "@stores", replacement: path.resolve(__dirname, "src/stores") },
+    // { find: "@widgets", replacement: path.resolve(__dirname, "src/widgets") },
+    ]
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
