ba08e0cdbc82e1b2310c983e7a0d8eefa553432e
Danishan Farookh <danishan089@gmail.com>
2026-04-19 19:47:53 +0530

    feat: Update the configuration

--- web-ui/src/layouts/workspace/layout/workspace.hook.ts
+++ web-ui/src/layouts/workspace/layout/workspace.hook.ts
@@ -8,6 +8,140 @@ export const useWorkspaceConfig = () => {
       center: [{ id: "t1", type: "text", text: "Home", route: "home" }],
       end: [{ id: "t1", type: "text", text: "Home", route: "home" }],
     },
+
+    [WS.LEFT_PRIMARY]: {
+      start: [
+        {
+          id: "t1",
+          type: "icon",
+          icon: "PartnershipIcon",
+          label: "Partnership",
+          route: "partnership",
+        },
+      ],
+      center: [],
+      end: [
+        {
+          id: "t12",
+          type: "icon",
+          icon: "homeIcon",
+          label: "Home Icon",
+          route: "home",
+        },
+      ],
+    },
+    [WS.LEFT_SIDEBAR]: [
+      {
+        id: "onbarding",
+        type: "group",
+        title: "Onboarding",
+        defaultOpen: true,
+        children: [
+          {
+            id: "onboarding-requests",
+            type: "label",
+            text: "Initial Inquiries",
+            leftIcons: [{ icon: "InquiryIcons" }],
+            route: "onboarding/requests",
+          },
+          {
+            id: "onboarding-review-information",
+            type: "label",
+            text: "Review Information",
+            leftIcons: [{ icon: "TransperancyIcon" }],
+            route: "onboarding/review-information",
+          },
+          {
+            id: "onboarding-negotiations",
+            type: "label",
+            text: "Negotiations",
+            leftIcons: [{ icon: "NegotiationIcon" }],
+            route: "onboarding/negotiations",
+          },
+          {
+            id: "onboarding-final-fit",
+            type: "label",
+            text: "Final Fit Assessment",
+            leftIcons: [{ icon: "FinalFitIcon" }],
+            route: "onboarding/final-fit",
+          },
+          {
+            id: "onboarding-selected",
+            type: "label",
+            text: "Selected",
+            leftIcons: [{ icon: "VerifiedIcon" }],
+            route: "onboarding/selected",
+          },
+          {
+            id: "onboarding-rejected",
+            type: "label",
+            text: "Rejected",
+            leftIcons: [{ icon: "RejectedIcon" }],
+            route: "onboarding/rejected",
+          },
+        ],
+      },
+      {
+        id: "onbarding-partner",
+        type: "group",
+        title: "Onboarding Partner",
+        defaultOpen: true,
+        children: [
+          {
+            id: "onboarding-p-requests",
+            type: "label",
+            text: "Request Form",
+            leftIcons: [{ icon: "editIcon" }],
+            route: "onboarding/requests",
+          },
+          {
+            id: "onboarding-p-application-status",
+            type: "label",
+            text: "Application Status",
+            leftIcons: [{ icon: "TransperancyIcon" }],
+            route: "onboarding/application-status",
+          },
+          {
+            id: "onboarding-p-information-gathering",
+            type: "label",
+            text: "Information Gathering",
+            leftIcons: [{ icon: "TransperancyIcon" }],
+            route: "onboarding/information-gathering",
+          },
+          {
+            id: "onboarding-p-negotiations",
+            type: "label",
+            text: "Negotiations",
+            leftIcons: [{ icon: "NegotiationIcon" }],
+            route: "onboarding/negotiations",
+          },
+          {
+            id: "onboarding-p-final-fit",
+            type: "label",
+            text: "Final Fit Assessment",
+            leftIcons: [{ icon: "FinalFitIcon" }],
+            route: "onboarding/final-fit",
+          },
+        ],
+      },
+    ],
+  };
+
+  return workspaceConfigDefaults;
+};
+
+/*
+
+import type { WorkspaceConfig } from "./layout.types";
+import { WORKSPACE_SLOTS as WS } from "../const/layout";
+
+export const useWorkspaceConfig = () => {
+  const workspaceConfigDefaults: WorkspaceConfig = {
+    [WS.TOP_PRIMARY]: {
+      start: [{ id: "t1", type: "text", text: "Home", route: "home" }],
+      center: [{ id: "t1", type: "text", text: "Home", route: "home" }],
+      end: [{ id: "t1", type: "text", text: "Home", route: "home" }],
+    },
     [WS.TOP_SECONDARY]: {
       start: [{ id: "t1", type: "text", text: "Home", route: "home" }],
     },
@@ -154,3 +288,7 @@ export const useWorkspaceConfig = () => {
 
   return workspaceConfigDefaults;
 };
+
+
+
+*/
