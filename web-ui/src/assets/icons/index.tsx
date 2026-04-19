da5f5a807ffcbf458eba9a3319d89c1c0dde168a
Danishan Farookh <danishan089@gmail.com>
2026-04-19 19:47:07 +0530

    feat: Added icons

--- web-ui/src/assets/icons/index.tsx
+++ web-ui/src/assets/icons/index.tsx
@@ -1,11 +1,13 @@
 import * as IconsTemp from "./icons";
 import * as Icons01 from "./list01";
 import * as Icons02 from "./list02";
+import * as Icons03 from "./list03";
 
 export const Icons = {
   ...IconsTemp,
   ...Icons01,
   ...Icons02,
+  ...Icons03,
 } as const;
 
 export type IconTypes = keyof typeof Icons;
--- web-ui/src/routes/WorkspaceRouter.tsx
+++ web-ui/src/routes/WorkspaceRouter.tsx
@@ -1,18 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Workspace } from "@/layouts/workspace";
 import { PageLogin, PageWorkspace } from "@/pages";
+import { TestIconGallery } from "@/tests";
 
 export const WorkspaceRouter = () => {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Routes>
         <Route path="/" element={<Navigate to="/workspace" replace />} />
+        <Route path="/icons" element={<TestIconGallery />} />
         <Route path="/login" element={<PageLogin />} />
         <Route path="/workspace" element={<Workspace />}>
           <Route index element={<PageWorkspace />} />
           <Route path="home" element={<> Home </>} />
           <Route path="settings" element={<> settings </>} />
           <Route path="profile" element={<> profile </>} />
+          <Route path="*" element={<PageWorkspace />} />
         </Route>
       </Routes>
     </div>
