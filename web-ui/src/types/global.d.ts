88e5d186e0f4e3ebc20423274782fd86914b4b31
Danishan Farookh <danishan089@gmail.com>
2026-04-29 13:00:55 +0530

    feat: Added styles in types

--- web-ui/src/types/global.d.ts
+++ web-ui/src/types/global.d.ts
@@ -2,3 +2,8 @@ declare module "core-ui";
 declare module "react-syntax-highlighter";
 declare module "unist";
 declare module "react-syntax-highlighter/dist/esm/styles/prism";
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
